6c9e6e831661364e0103ec387c6299539482bc47
sunayanajindal <89250105+sunayanajindal@users.noreply.github.com>
2022-02-19 17:59:13 +0530

    Changes done in EndGameReport to accomodate new classes

---
+++ src/EndGameReportMemberPanel.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.util.Iterator;
+import java.util.Vector;
+
+public class EndGameReportMemberPanel {
+    private JList memberList;
+    JPanel partyPanel;
+    public EndGameReportMemberPanel(Party party){
+        partyPanel = new JPanel();
+        partyPanel.setLayout(new FlowLayout());
+        partyPanel.setBorder(new TitledBorder("Party Members"));
+
+        Vector myVector = new Vector();
+        Iterator iter = (party.getMembers()).iterator();
+        while (iter.hasNext()){
+            myVector.add( ((Bowler)iter.next()).getNick() );
+        }
+        memberList = new JList(myVector);
+        memberList.setFixedCellWidth(120);
+        memberList.setVisibleRowCount(5);
+        //memberList.addListSelectionListener(this);
+        JScrollPane partyPane = new JScrollPane(memberList);
+        //        partyPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        partyPanel.add(partyPane);
+
+        partyPanel.add( memberList );
+    }
+
+    public JList getMemberList() {
+        return memberList;
+    }
+
+    public JPanel getPartyPanel() {
+        return partyPanel;
+    }
+}
